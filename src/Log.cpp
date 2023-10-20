c1e610ca75635171d873679756fdc768396e3aab
gnilk <wef@wef.com>
2023-10-20 18:46:44 +0200

    CHG: Replaced vprintf with fmt
    Varadic C function prototypes are quite old. Spent some time to understand packed function arguments and then decided
    to use {fmt} instead.

--- src/Log.cpp
+++ src/Log.cpp
@@ -8,6 +8,18 @@
 
 using namespace gnilk;
 
+Log::Log(const std::string &logName) : name(logName) {
+
+}
+
+void Log::Initialize() {
+
+}
+
+Log::~Log() {
+
+}
+
 Log::Ref Log::Create(const std::string &logName) {
     auto ptrInst = new Log(logName);
     ptrInst->Initialize();
@@ -15,45 +27,8 @@ Log::Ref Log::Create(const std::string &logName) {
     return ref;
 }
 
-Log::Log(const std::string &logName) : name(logName) {
 
-}
-
-void Log::Initialize() {
-}
-
-Log::~Log() {
-}
-
-/*
-// public:
-void Log::WriteLine(const std::string &format, ...) {
-
-}
-void Log::WriteLine(LogLevel logLevel, const std::string &format,...) {
-
-}
-void Log::Critical(const std::string &format,...) {
-
-}
-void Log::Error(const std::string &format, ...) {
-
-}
-void Log::Warning(const std::string &format, ...) {
-
-}
-void Log::Info(const std::string &format, ...) {
-
-}
-*/
-
-FILE *Log::GetWriteStream() {
-    return LogManager::Instance().GetLogEventPipe().GetWriteStream();
-}
-
-
-
-void Log::GenerateEventData(LogEvent &outEvent, LogLevel level) {
+void Log::GenerateAndSendEventData(LogEvent &outEvent, LogLevel level) const {
     outEvent.timeStamp = LogClock::now();
     outEvent.level = level;
     outEvent.idSenderThread = std::this_thread::get_id();
@@ -61,32 +36,10 @@ void Log::GenerateEventData(LogEvent &outEvent, LogLevel level) {
     outEvent.Write();
 }
 
-// This is not supported (va_start can't take reference) - however, GCC + Clang - correctly compiles this but MSVC throws an error
-void Log::DebugRef(const std::string &format, ...) {
-    va_list values;
+void Log::SendLogMessage(LogLevel level, const std::string &dbgMsg) const {
     LogEvent logEvent;
-    GenerateEventData(logEvent, kDebug);
-
-    va_start( values, format);
-    vfprintf(GetWriteStream(), format.c_str(), values);
-    va_end(    values);
-
-    // Must flush data on the pipe for this to work...
-    fflush(GetWriteStream());
-    LogManager::Instance().SendToSinks();
-}
-
-void Log::Debug(const char *format, ...) {
-    va_list values;
-    LogEvent logEvent;
-    GenerateEventData(logEvent, kDebug);
-
-    va_start( values, format);
-    vfprintf(GetWriteStream(), format, values);
-    va_end(    values);
-
-    // Must flush data on the pipe for this to work...
-    fflush(GetWriteStream());
+    GenerateAndSendEventData(logEvent, level);
+    logEvent.WriteMsgString(dbgMsg);
     LogManager::Instance().SendToSinks();
 }
 
--- src/Log.h
+++ src/Log.h
@@ -9,6 +9,9 @@
 #include <string>
 #include <stdio.h>
 
+#include "fmt/printf.h"
+#include "fmt/format.h"
+
 #include "LogCore.h"
 #include "LogEvent.h"
 
@@ -21,13 +24,76 @@ namespace gnilk {
         static Log::Ref Create(const std::string &logName);
         virtual ~Log();
 
-        void DebugRef(const std::string &format, ...);
-        void Debug(const char *format, ...);
+        //
+        // New and improved, using fmt - see: https://fmt.dev/latest/syntax.html
+        //
+        template <class...T>
+        inline void Dbg(T... arguments) {
+            auto str = fmt::format(arguments...);
+            SendLogMessage(kDebug, str);
+        }
+        template <class...T>
+        inline void Inf(T... arguments) {
+            auto str = fmt::format(arguments...);
+            SendLogMessage(kInfo, str);
+        }
+        template <class...T>
+        inline void Warn(T... arguments) {
+            auto str = fmt::format(arguments...);
+            SendLogMessage(kWarning, str);
+        }
+        template <class...T>
+        inline void Err(T... arguments) {
+            auto str = fmt::format(arguments...);
+            SendLogMessage(kError, str);
+        }
+        template <class...T>
+        inline void Crit(T... arguments) {
+            auto str = fmt::format(arguments...);
+            SendLogMessage(kCritical, str);
+        }
+
+        //
+        // these all use printf formatting - old legacy
+        //
+        template <class...T>
+        inline void Debug(T... arguments) const {
+            auto str = fmt::sprintf(arguments...);
+            SendLogMessage(kDebug, str);
+        }
+
+        template <class...T>
+        inline void Info(T... arguments) const {
+            auto str = fmt::sprintf(arguments...);
+            SendLogMessage(kInfo, str);
+        }
+
+        template <class...T>
+        inline void Warning(T... arguments) const {
+            auto str = fmt::sprintf(arguments...);
+            SendLogMessage(kWarning, str);
+        }
+
+        template <class...T>
+        inline void Error(T... arguments) const {
+            auto str = fmt::sprintf(arguments...);
+            SendLogMessage(kError, str);
+        }
+
+        template <class...T>
+        inline void Critical(T... arguments) const {
+            auto str = fmt::sprintf(arguments...);
+            SendLogMessage(kCritical, str);
+        }
+
     protected:
+        void SendLogMessage(LogLevel level, const std::string &dbgMsg) const;
+
+    protected:
+
         explicit Log(const std::string &logName);
         void Initialize();
-        void GenerateEventData(LogEvent &outEvent, LogLevel level);
-        FILE *GetWriteStream();
+        void GenerateAndSendEventData(LogEvent &outEvent, LogLevel level) const;
     private:
         std::string name;
     };
