d8df9e384579892e38124c3457ecb513b800ef1f
gnilk <1664069+gnilk@users.noreply.github.com>
2023-10-24 14:48:05 +0200

    NEW: Log's have loglevel filtering
    Both sinks and logs have log-level filtering. This allows fine-tuning of where certain stuff goes.
    Note however that once a message is filtered it won't make down.
    Thus, if the LOG filters > DEBUG (like kInfo), no DEBUG message will reach any sink regardless of their setting.

--- src/Log.h
+++ src/Log.h
@@ -31,36 +31,43 @@ namespace gnilk {
             return isEnabled;
         }
 
+        void SetLogLevelThreshold(LogLevel newThreshold) {
+            logLevelThreshold = newThreshold;
+        }
+        LogLevel GetLogLevelThreshold() const {
+            return logLevelThreshold;
+        }
+
         //
         // New and improved, using fmt - see: https://fmt.dev/latest/syntax.html
         //
         template <class...T>
         inline void Dbg(T... arguments) {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kDebug))) return;
             auto str = fmt::format(arguments...);
             SendLogMessage(kDebug, str);
         }
         template <class...T>
         inline void Inf(T... arguments) {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kInfo))) return;
             auto str = fmt::format(arguments...);
             SendLogMessage(kInfo, str);
         }
         template <class...T>
         inline void Warn(T... arguments) {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kWarning))) return;
             auto str = fmt::format(arguments...);
             SendLogMessage(kWarning, str);
         }
         template <class...T>
         inline void Err(T... arguments) {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kError))) return;
             auto str = fmt::format(arguments...);
             SendLogMessage(kError, str);
         }
         template <class...T>
         inline void Crit(T... arguments) {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kCritical))) return;
             auto str = fmt::format(arguments...);
             SendLogMessage(kCritical, str);
         }
@@ -70,35 +77,35 @@ namespace gnilk {
         //
         template <class...T>
         inline void Debug(T... arguments) const {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kDebug))) return;
             auto str = fmt::sprintf(arguments...);
             SendLogMessage(kDebug, str);
         }
 
         template <class...T>
         inline void Info(T... arguments) const {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kInfo))) return;
             auto str = fmt::sprintf(arguments...);
             SendLogMessage(kInfo, str);
         }
 
         template <class...T>
         inline void Warning(T... arguments) const {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kWarning))) return;
             auto str = fmt::sprintf(arguments...);
             SendLogMessage(kWarning, str);
         }
 
         template <class...T>
         inline void Error(T... arguments) const {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kError))) return;
             auto str = fmt::sprintf(arguments...);
             SendLogMessage(kError, str);
         }
 
         template <class...T>
         inline void Critical(T... arguments) const {
-            if (!isEnabled) return;
+            if (!(isEnabled && WithinRange(kCritical))) return;
             auto str = fmt::sprintf(arguments...);
             SendLogMessage(kCritical, str);
         }
@@ -111,9 +118,14 @@ namespace gnilk {
         explicit Log(const std::string &logName);
         void Initialize();
         void GenerateAndSendEventData(LogEvent &outEvent, LogLevel level) const;
+        __inline bool WithinRange(LogLevel msgLevel) const {
+            return ((msgLevel >= logLevelThreshold)?true: false);
+        }
+
     private:
         std::string name = {};
         bool isEnabled = true;
+        LogLevel logLevelThreshold = LogLevel::kNone;   // Everything, no threshold
     };
 
     // Legacy - for now...
