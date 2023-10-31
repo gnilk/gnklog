74800a7ef8ebbf5151f658f5afd74c62d2dca8ef
gnilk <1664069+gnilk@users.noreply.github.com>
2023-10-31 08:53:37 +0100

    FIX: Updated unit test for log manager

--- src/utests/test_logmanager.cpp
+++ src/utests/test_logmanager.cpp
@@ -13,6 +13,32 @@ DLL_EXPORT int test_logmanager(ITesting *t);
 }
 
 DLL_EXPORT int test_logmanager(ITesting *t) {
+    // The test here - it should work and not crash!
+    LogManager::Instance().Initialize();
+    auto logA = Logger::GetLog("logA");
+    logA->Debug("hello");
     LogManager::Instance().Close();
+    TR_ASSERT(t, Log::kStatus::kSendError == logA->Debug("closed"));
+
+    // Now - logA will be 'orphaned' - i.e. unknown to the log-manager...
+    LogManager::Instance().Initialize();
+    TR_ASSERT(t, Log::kStatus::kOk == logA->Debug("Reopened"));
+    LogManager::Instance().Close();
+
+    int logCounter = 0;
+    LogManager::Instance().IterateLogs([&logCounter](const Log::Ref &log) ->bool {
+        logCounter++;
+        return true;
+    });
+    // No logs should be present after this - or should they????
+    TR_ASSERT(t, logCounter == 0);
+
+    // This should simply not crash..
+    LogManager::Instance().Initialize();
+    LogManager::Instance().Close();
+
+    LogManager::Instance().Initialize();
+    LogManager::Instance().Close();
+
     return kTR_Pass;
 }
